fdbe006ec7bcbe88ce648721e381fe0624608453
Ian MacDonald <ianmacdonald@Ian-MacDonalds-MacBook-Pro.local>
2018-08-07 18:27:34 -0230

    react work

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,59 +1,75 @@
 import React from 'react';
-import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
-import IconButton from '@material-ui/core/IconButton';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 
 class Nav extends React.Component {
-    state = {
-        anchorEl: null,
-      };
-    
-      handleClick = event => {
-        this.setState({ anchorEl: event.currentTarget });
-      };
-    
-      handleClose = () => {
-        this.setState({ anchorEl: null });
-      };
+  state = {
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  };
+
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
+  };
+
+  render() {
+    const sideList = (
+      <div className="list">
+        <List>
+          <ListItem button component="a" href="">
+          <ListItemText primary="login" />
+          </ListItem>
+        </List>
+      </div>
+    );
 
 
-render() {
-    const { anchorEl } = this.state;
-    return(
-        <div className="root">
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton 
-                    className="menuButton" 
-                    color="inherit" 
-                    aria-label="Menu"
-                    aria-owns={anchorEl ? 'simple-menu' : null}
-                    aria-haspopup="true"
-                    onClick={this.handleClick}
-                    >
-                    <MenuIcon />
-                    </IconButton>
-                    <Menu
-                    id="simple-menu"
-                    anchorEl={anchorEl}
-                    open={Boolean(anchorEl)}
-                    onClose={this.handleClose}
-                    >
-                    <MenuItem onClick={this.handleClose}>Login</MenuItem>
-                    </Menu>
-                    <Typography variant="title" color="inherit" className="titleName">
-                    East Coast Trail
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </div>
-        );
-    }
+
+    return (
+      <div>
+      <div className="root">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton 
+          className="menuButton" 
+          color="inherit" 
+          aria-label="Menu"
+          onClick={this.toggleDrawer('left', true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title" color="inherit" className="flex">
+            News
+          </Typography>
+        </Toolbar>
+      </AppBar>
+       <Drawer open={this.state.left} onClose={this.toggleDrawer('left', false)}>
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer('left', false)}
+            onKeyDown={this.toggleDrawer('left', false)}
+          >
+            {sideList}
+          </div>
+       </Drawer>
+      </div>
+      </div>
+    );
+  }
 }
 
-export default Nav;
+
+export default  Nav;
